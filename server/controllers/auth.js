1d1acb350f15e1079ffcabe0289520768d2b5841
KGeo123 <kyriacosgeorgiou8@gmail.com>
2021-08-22 22:19:53 +0300

    added the refresh access token functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import { hash, compare } from 'bcrypt';
 import User from '../models/user.js';
 import { throwError } from '../lib/errors.js';
 import { generateAccessToken, generateRefreshToken } from '../lib/tokens.js';
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
   const { email, password, username } = req.body;
@@ -48,11 +49,49 @@ export const login = async (req, res, next) => {
     }
     res.cookie('refresh_token', newRefreshToken, {
       httpOnly: true,
-      path: '/refresh_token'
+      path: '/auth/refresh-token'
     });
     res
       .status(200)
       .json({ message: 'successfully logged in', accessToken: newAccessToken });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshAccessToken = async (req, res, next) => {
+  console.log(req.cookies);
+  const refreshToken = req.cookies.refresh_token;
+  try {
+    const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const user = await User.findById(payload.userId);
+    if (!user) {
+      throwError('You are not authorized', 401);
+    }
+    if (user.refreshToken !== refreshToken) {
+      throwError('You are not authorized', 401);
+    }
+    const newRefreshToken = generateRefreshToken(user._id.toString());
+    const newAccessToken = generateAccessToken({
+      email: user.email,
+      userId: user._id.toString()
+    });
+    res.cookie('refresh_token', newRefreshToken, {
+      httpOnly: true,
+      path: '/auth/refresh-token'
+    });
+    user.refreshToken = newRefreshToken;
+    const updatedUser = await user.save();
+    console.log(updatedUser);
+    if (!updatedUser) {
+      throwError();
+    }
+    res
+      .status(200)
+      .json({
+        message: 'successfully generated new access token',
+        accessToken: newAccessToken
+      });
   } catch (error) {
     next(error);
   }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
-import express from "express";
-import * as AuthController from '../controllers/auth.js'
+import express from 'express';
+import * as AuthController from '../controllers/auth.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', AuthController.signUp);
 
 authRouter.post('/login', AuthController.login);
+
+authRouter.get('/refresh-token', AuthController.refreshAccessToken);
 
 export default authRouter;
