ed0b7d06bf785c8db52cffa151dcaba02019e6c7
KGeo123 <kyriacosgeorgiou8@gmail.com>
2021-08-22 17:09:47 +0300

    added the error handling middleware and the 404 handler middleware

--- server/app.js
+++ server/app.js
@@ -17,6 +17,17 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'no endpoint found' });
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500).json({
+    message: error.message || 'internal server error',
+    ...(error.description ? { description: error.description } : null)
+  });
+});
+
 try {
   await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
