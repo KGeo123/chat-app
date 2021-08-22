95742a773a0b7e3e7cbf14e246a171735f04106e
KGeo123 <kyriacosgeorgiou8@gmail.com>
2021-08-22 17:25:03 +0300

    added the user model

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  refreshToken: {
+    type: String,
+    required: false
+  }
+});
+
+export const User = model('User', userSchema);
