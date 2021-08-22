f5a2a1d7cab007ced97f8eb580c5e93398ac9255
KGeo123 <kyriacosgeorgiou8@gmail.com>
2021-08-22 18:18:58 +0300

    fixed bug: a user could be stored again with the same email after signup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,21 +3,21 @@ import User from '../models/user.js';
 import { throwError } from '../lib/errors.js';
 
 export const signUp = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, username } = req.body;
   const hashedPassword = await hash(password, 16);
-  const user = new User({ email, password: hashedPassword });
+  const user = new User({ email, password: hashedPassword, username });
   try {
     const doesUserExistWithThisEmail = !!(await User.findOne({ email }));
-    const savedUser = await user.save();
-    if (!savedUser) {
-      throwError('Could not save new user');
-    }
     if (doesUserExistWithThisEmail) {
-      throwError('the specified email is already in use', 409);
+			throwError('the specified email is already in use', 409);
+    }
+		const savedUser = await user.save();
+    if (!savedUser) {
+			throwError('Could not save new user');
     }
     res.status(201).json({
       message: 'Successfully created user',
-      user: { userId: savedUser._id.toString(), email: savedUser.email }
+      user: { email: savedUser.email, username: savedUser.username }
     });
   } catch (error) {
     next(error);
