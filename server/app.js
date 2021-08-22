42f69bc67e6a0ab805a312eeb8237e0f14ba6064
KGeo123 <kyriacosgeorgiou8@gmail.com>
2021-08-22 17:05:46 +0300

    created the connection with the database using mongoose

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
 app.use(
   cors({
@@ -11,3 +15,14 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cookieParser());
+
+try {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const server = app.listen(process.env.PORT || 4000);
+} catch (error) {
+  console.log(error);
+}
